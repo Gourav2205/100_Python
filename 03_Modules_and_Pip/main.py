5bc9b356178fe812dd8f03ea7cb297866d480147
Gourav2205 <gouravbiswas897@gmail.com>
2023-07-24 10:49:56 +0530

    First

---
+++ 03_Modules_and_Pip/main.py
@@ -0,0 +1,9 @@
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi")
+
+# Dont worry about how to use these modules just yet!
+p = pandas.read_csv("one.csv")
+print(p)
+m = hashlib.sha256()
